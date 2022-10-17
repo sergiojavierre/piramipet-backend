3f6cdaa04d1224a8950c14c9f9fbe66af44f567d
sergiojavierre <sergiojavierre91@gmail.com>
2022-10-17 08:49:44 +0200

    Inicial

---
+++ src/main/java/com/cpifppiramide/piramipet/piramipet/PiramipetApplication.java
@@ -0,0 +1,13 @@
+package com.cpifppiramide.piramipet.piramipet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiramipetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiramipetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpifppiramide/piramipet/piramipet/PiramipetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpifppiramide.piramipet.piramipet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiramipetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
